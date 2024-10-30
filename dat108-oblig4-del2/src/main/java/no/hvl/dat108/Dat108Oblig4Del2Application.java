3ab8d9c46f360510ced19d768c7d2ebba8d27931
h678278 <damjonatan@gmail.com>
2024-10-30 13:37:44 +0100

    Oppsett av skjelett

---
+++ dat108-oblig4-del2/src/main/java/no/hvl/dat108/Dat108Oblig4Del2Application.java
@@ -0,0 +1,13 @@
+package no.hvl.dat108;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat108Oblig4Del2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat108Oblig4Del2Application.class, args);
+	}
+
+}
